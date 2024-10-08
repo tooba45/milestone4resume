f1c5119d2edd48950d51fdc79a3dc87f315f45dd
tooba45 <162182611+tooba45@users.noreply.github.com>
2024-10-08 10:09:45 +0500

    Add files via upload

---
+++ milestone4/script.js
@@ -0,0 +1,66 @@
+var _a;
+//listing element
+(_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    var _a;
+    event.preventDefault();
+    //type assertion
+    var profilePictureInput = document.getElementById('profilePicture');
+    var nameElement = document.getElementById('name');
+    var emailElement = document.getElementById('email');
+    var phoneElement = document.getElementById('phone');
+    var educationElement = document.getElementById('education');
+    var experienceElement = document.getElementById('experience');
+    var skillsElement = document.getElementById('skills');
+    Element;
+    //Check if all form elements are present
+    if (profilePictureInput && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+        var name_1 = nameElement.value;
+        var email = emailElement.value;
+        var phone = phoneElement.value;
+        var education = educationElement.value;
+        var experience = experienceElement.value;
+        var skills = skillsElement.value;
+        //picture elements
+        var profilePictureFile = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
+        var profilePictureURl = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
+        //Create Resume output
+        var resumeOutput = "\n <h2> Editable Resume</h2>\n ".concat(profilePictureURl ? "<img src=".concat(profilePictureURl, " alt=\"Profile Picture\" class=\"profilePicture\">") : '', "\n <p><strong>Name:</strong> <span id=\"edit-name\" class=\"editable\"> ").concat(name_1, " </span> </p>\n <p><strong>Email:</strong><span id=\"edit-email\" class=\"editable\"> ").concat(email, "  </span> </p>\n <p><strong>Phone:</strong> <span id=\"edit-phone\" class=\"editable\">").concat(phone, "  </span> </p>\n\n <h3>Education</h3>\n <p id=\"edit-education\" class=\"editable\">").concat(education, "</p>\n\n  <h3>Experience</h3>\n <p id=\"edit-experience\" class=\"editable\">").concat(experience, " </p>\n\n  <h3>Skills</h3>\n <p id=\"edit-skills\" class=\"editable\">").concat(skills, " \n \n </p>\n ");
+        //Display the resume output
+        var resumeOutputElement = document.getElementById('resumeOutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+            makeEditable();
+        }
+        else {
+            console.error('the resume output element are missing');
+        }
+    }
+    else {
+        console.error('one or more output elements are missing');
+    }
+});
+function makeEditable() {
+    var editableElements = document.querySelectorAll('.editable');
+    editableElements.forEach(function (element) {
+        element.addEventListener('click', function () {
+            var _a;
+            var currentElement = element;
+            var currentValue = currentElement.textContent || "";
+            //replace content
+            if (currentElement.tagName === "P" || currentElement.tagName === 'SPAN') {
+                var input_1 = document.createElement('input');
+                input_1.type = 'text';
+                input_1.value = currentValue;
+                input_1.classList.add('editing input');
+                input_1.addEventListener('blur', function () {
+                    currentElement.textContent = input_1.value;
+                    currentElement.style.display = 'inline';
+                    input_1.remove();
+                });
+                currentElement.style.display = 'none';
+                (_a = currentElement.parentNode) === null || _a === void 0 ? void 0 : _a.insertBefore(input_1, currentElement);
+                input_1.focus();
+            }
+        });
+    });
+}
